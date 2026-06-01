eb4da172ea6148fdd5523959f8b0b17cbff9e207
RenPT23 <161973086+RenPT23@users.noreply.github.com>
2026-06-01 21:42:33 +0100

    Add LegacyAMD device implementation

--- intern/cycles/device/legacyamd/device.cpp
+++ intern/cycles/device/legacyamd/device.cpp
@@ -1,26 +1,27 @@
 /* SPDX-FileCopyrightText: 2011-2022 Blender Foundation
  *
- * SPDX-License-Identifier: Apache-2.0 
+ * SPDX-License-Identifier: Apache-2.0
  * Modified by RenPT/RenPT23/RenPT23R
- * Added LegacyAMD */
+ * Added LegacyAMD backend */
 
-#include "device/dummy/device.h"
+#include "device/legacyamd/device.h"
 #include "device/device.h"
 #include "device/queue.h"
 
 CCL_NAMESPACE_BEGIN
 
-/* Dummy device for when creating an appropriate rendering device fails. */
-
-class DummyDevice : public Device {
+class LegacyAMDDevice : public Device {
  public:
-  DummyDevice(const DeviceInfo &info_, Stats &stats_, Profiler &profiler_, bool headless_)
+  LegacyAMDDevice(const DeviceInfo &info_,
+                  Stats &stats_,
+                  Profiler &profiler_,
+                  bool headless_)
       : Device(info_, stats_, profiler_, headless_)
   {
     error_msg = info.error_msg;
   }
 
-  ~DummyDevice() override = default;
+  ~LegacyAMDDevice() override = default;
 
   BVHLayoutMask get_bvh_layout_mask(uint /*kernel_features*/) const override
   {
@@ -28,29 +29,31 @@ class DummyDevice : public Device {
   }
 
   void mem_alloc(device_memory & /*mem*/) override {}
-
   void mem_copy_to(device_memory & /*mem*/) override {}
-
   void mem_move_to_host(device_memory & /*mem*/) override {}
 
-  void mem_copy_from(
-      device_memory & /*mem*/, size_t /*y*/, size_t /*w*/, size_t /*h*/, size_t /*elem*/) override
+  void mem_copy_from(device_memory & /*mem*/,
+                     size_t /*y*/,
+                     size_t /*w*/,
+                     size_t /*h*/,
+                     size_t /*elem*/) override
   {
   }
 
   void mem_zero(device_memory & /*mem*/) override {}
-
   void mem_free(device_memory & /*mem*/) override {}
 
-  void const_copy_to(const char * /*name*/, void * /*host*/, size_t /*size*/) override {}
+  void const_copy_to(const char * /*name*/,
+                      void * /*host*/,
+                      size_t /*size*/) override {}
 };
 
-unique_ptr<Device> device_dummy_create(const DeviceInfo &info,
-                                       Stats &stats,
-                                       Profiler &profiler,
-                                       bool headless)
+unique_ptr<Device> device_legacy_amd_create(const DeviceInfo &info,
+                                             Stats &stats,
+                                             Profiler &profiler,
+                                             bool headless)
 {
-  return make_unique<DummyDevice>(info, stats, profiler, headless);
+  return make_unique<LegacyAMDDevice>(info, stats, profiler, headless);
 }
 
 CCL_NAMESPACE_END
